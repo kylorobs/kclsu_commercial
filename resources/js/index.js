8c6833770d8ba378f7acbba0b3b8028b9f65a9ae
Kyle <Kyle@idea-PC.com>
2018-06-11 07:59:50 +0100

    Add Jumbotron and Sticky Nav

---
+++ resources/js/index.js
@@ -0,0 +1,27 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  stickNav()
+};
+
+// Get the navbar
+var navbar = document.getElementById("stickynav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+/*
+function fadeNav(){
+  if (navbar.hasClass(".sticky"){
+    $('.nav li').addclass('opacity', '1');
+  })
+}
+*/
