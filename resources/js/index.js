03a3535ee0f49f6447f95655b9bb2b88ae13e663
Kyle Robinson <KyleR@kclsu.org>
2018-06-22 17:41:42 +0100

    javascript dates bold

--- resources/js/index.js
+++ resources/js/index.js
@@ -35,7 +35,12 @@ function stickNav() {
 const currentDate = new Date();
 const currentDay = currentDate.getDay();
 
-if (currentDay === 2) {
+if (currentDay === 1) {
+ document.getElementById('mon').style.color = 'purple';
+ document.getElementById('mon').style.fontWeight = 'bold';
+}
+
+else if (currentDay === 2) {
  document.getElementById('tues').style.color = 'purple';
  document.getElementById('tues').style.fontWeight = 'bold';
 }
@@ -43,6 +48,26 @@ if (currentDay === 2) {
 else if (currentDay === 3) {
  document.getElementById('weds').style.color = 'purple';
  document.getElementById('weds').style.fontWeight = 'bold';
+};
+
+else if (currentDay === 4) {
+ document.getElementById('thurs').style.color = 'purple';
+ document.getElementById('thurs').style.fontWeight = 'bold';
+};
+
+else if (currentDay === 5) {
+ document.getElementById('fri').style.color = 'purple';
+ document.getElementById('fri').style.fontWeight = 'bold';
+};
+
+else if (currentDay === 6) {
+ document.getElementById('sat').style.color = 'purple';
+ document.getElementById('sat').style.fontWeight = 'bold';
+};
+
+else if (currentDay === 7) {
+ document.getElementById('sun').style.color = 'purple';
+ document.getElementById('sun').style.fontWeight = 'bold';
 };
 
 });
